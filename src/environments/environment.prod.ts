5d285b80b3ca002eacb49b38f4439a24d6981f06
JuanSCaicedo <sebanchit@gmail.com>
2024-05-16 23:02:41 -0500

    configurando AuthService.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -156,7 +156,7 @@ export const environment = {
       'thumbnail': './assets/media/demos/demo20.png'
     },
   },
-  URL_BACKEND: 'http://127.0.0.1:8000/',
+  URL_BACKEND: 'http://127.0.0.1:8000',
   URL_SERVICIOS: 'http://127.0.0.1:8000/api',
   URL_FRONTEND: 'http://localhost:5000',
 };
