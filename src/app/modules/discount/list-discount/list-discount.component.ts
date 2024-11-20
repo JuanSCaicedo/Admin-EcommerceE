565171b9e6213ec22da8c1cb3893b0f081c77754
JuanScaicedo <sebanchit@gmail.com>
2024-11-19 22:44:50 -0500

    listado de campaña de descuentos

--- src/app/modules/discount/list-discount/list-discount.component.ts
+++ src/app/modules/discount/list-discount/list-discount.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ToastrService } from 'ngx-toastr';
+import { DiscountService } from '../service/discount.service';
+import { DeleteDiscountComponent } from '../delete-discount/delete-discount.component';
 
 @Component({
   selector: 'app-list-discount',
@@ -7,4 +11,89 @@ import { Component } from '@angular/core';
 })
 export class ListDiscountComponent {
 
+  discounts: any = [];
+  search: string = '';
+  totalPages: number = 0;
+  currentPage: number = 1;
+
+  isLoading$: any;
+
+  constructor(
+    public discountService: DiscountService,
+    public modalService: NgbModal,
+    public toastr: ToastrService,
+  ) { }
+
+  ngOnInit(): void {
+    this.listDiscounts();
+    this.isLoading$ = this.discountService.isLoading$;
+  }
+
+  listDiscounts(page = 1) {
+    this.discountService.listDiscounts(page, this.search).subscribe((resp: any) => {
+      console.log(resp);
+      this.discounts = resp.discounts.data;
+      this.totalPages = resp.total;
+      this.currentPage = page;
+    }, (error: any) => {
+      console.log(error);
+      this.toastr.error('API Response - Comuniquese con el desarrollador', error.error.message || error.message);
+    });
+  }
+
+  searchTo() {
+    this.listDiscounts();
+  }
+
+  loadPage($event: any) {
+    console.log($event);
+    this.listDiscounts($event);
+  }
+
+  getNameTypeDiscount(discount_type: number) {
+    let NAME = '';
+
+    switch (discount_type) {
+      case 1:
+        NAME = 'Productos';
+        break;
+      case 2:
+        NAME = 'Categorias';
+        break;
+      case 3:
+        NAME = 'Marcas';
+    }
+
+    return NAME;
+  }
+  
+  getNameTypeCampaing(type_campaign: number) {
+    let NAME = '';
+
+    switch (type_campaign) {
+      case 1:
+        NAME = 'Campaña normal';
+        break;
+      case 2:
+        NAME = 'Campaña flash';
+        break;
+      case 3:
+        NAME = 'Campaña link';
+    }
+
+    return NAME;
+  }
+
+
+  deleteDiscount(discount: any) {
+    const modalRef = this.modalService.open(DeleteDiscountComponent, { centered: true, size: 'md' });
+    modalRef.componentInstance.discount = discount;
+
+    modalRef.componentInstance.DiscountD.subscribe((resp: any) => {
+      let INDEX = this.discounts.findIndex((item: any) => item.id == discount.id);
+      if (INDEX != -1) {
+        this.discounts.splice(INDEX, 1)
+      }
+    });
+  }
 }
